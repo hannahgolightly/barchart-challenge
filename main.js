692adddd6ee9466ff9046b905ff783781794e9ff
Hannah Golightly <hannah.golightly@hotmail.co.uk>
2022-06-23 21:38:58 +0100

    tidying code

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
 import { default as data } from "./data.json" assert { type: "json" };
 
+const chart = document.querySelector('.chart');
 const highest = Math.max(...data.map(d => d.amount))
 const scale = 2;
-console.log(highest)
-
-const chart = document.querySelector('.chart');
 let totalAmount = 0;
+
 //for each day loop over and create a div, append to the parent chart
 data.map(date => {
     const barArea = document.createElement('div');
-    barArea.setAttribute('class', 'bar-area');
     const day = document.createElement('p');
     const bar = document.createElement('div');
     const amount = document.createElement('div');
     amount.setAttribute('class', 'amount');
+    barArea.setAttribute('class', 'bar-area');
     barArea.appendChild(amount)
     barArea.appendChild(bar)
     barArea.appendChild(day)
     chart.appendChild(barArea)
+
     amount.textContent = `$${date.amount}`;
     day.textContent = date.day;
     const barHeight = date.amount * scale;
+
     bar.style.height = `${barHeight}px`;
     bar.classList.add('bar');
     if (date.amount === highest) {
